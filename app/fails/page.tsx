fb53d7352cb46a322ba9fab0a3dd40d277370df8
Kelly Littlepage <kelly.littlepage@gmail.com>
2024-12-31 14:10:25 -0700

    feat: minimal repo of the trailing slash rcs bug

---
+++ app/fails/page.tsx
@@ -0,0 +1,9 @@
+import Link from "next/link";
+
+export default function Home() {
+  return (
+    <div>
+      <Link href="/">Link to root</Link>
+    </div>
+  );
+}
---
+++ next.config.mjs
@@ -0,0 +1,11 @@
+/**
+ * @type {import('next').NextConfig}
+ */
+const nextConfig = {
+  output: 'export',
+  trailingSlash: true,
+  skipTrailingSlashRedirect: true,
+  reactStrictMode: true,
+};
+
+export default nextConfig;
